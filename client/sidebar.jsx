dbb523648020f293e9dff8708df240724ea6d585
Joshua Marx <marx.joshua@gmail.com>
2018-12-03 15:41:44 -0800

    sidebar module

---
+++ client/sidebar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Sidebar extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="sidebar">
+        <h2>Sidebar</h2>
+      </div>
+    );
+  }
+};
