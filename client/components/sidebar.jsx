c0b149c9b9755683da7353136413c59c405cc074
Joshua Marx <marx.joshua@gmail.com>
2018-12-03 21:17:05 -0800

    added id

---
+++ client/components/sidebar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Sidebar extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <label id="report">Reports</label>
+      </div>
+    );
+  }
+};
