f79e6a24326818fa264e6400ebaec94cbbf4e736
Joshua Marx <marx.joshua@gmail.com>
2018-12-03 21:17:15 -0800

    created topbar component

---
+++ client/components/topbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Sidebar extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <label id="report">CityReport</label>
+      </div>
+    );
+  }
+};
