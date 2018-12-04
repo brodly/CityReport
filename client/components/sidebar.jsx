b1df7d143c0b1b39066b1f1371647c17843b28fb
Joshua Marx <marx.joshua@gmail.com>
2018-12-03 21:45:22 -0800

    added dropdown and submission form

--- client/components/sidebar.jsx
+++ client/components/sidebar.jsx
@@ -11,8 +11,23 @@ export default class Sidebar extends Component {
   render() {
     return (
       <div>
-        <label id="report">Reports</label>
+        <div>
+          <label id="report">Reports</label>
+        </div>
+        <select>
+          <option value="pothole">Pothole</option>
+          <option value="hydrant">Fire Hydrant</option>
+          <option value="lamppost">Lamp Post</option>
+          <option value="manhole">Manhole</option>
+        </select>
+        <br/>
+        <form>
+          First name: <input type="text" name="fname"/><br/>
+          Last name: <input type="text" name="lname"/><br/>
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
+      
     );
   }
 };
