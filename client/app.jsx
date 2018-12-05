9e3bcedde82cd1636ddafd2274f3248bfb0619d1
Joshua Marx <marx.joshua@gmail.com>
2018-12-04 19:10:15 -0800

    added fetch data and set data state from server

--- client/app.jsx
+++ client/app.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
-
+import axios from 'axios';
 import Map from './components/map';
 import Sidebar from './components/sidebar';
 import Topbar from './components/topbar';
+import Report from './components/report';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    }
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData(val) {
+    axios.get(`/${val}`)
+      .then((res) => {
+        this.setState({
+          data: res.data,
+        })
+      })
+      .catch(err => { throw err });
+  }
+
+  componentDidMount() {
+    this.fetchData(1);
+  }
+
   render() {
+    const { data } = this.state;
+    
     return (
       <div>
         <div className="topbar">
@@ -13,6 +38,8 @@ export default class App extends React.Component {
         </div>
         <div className="sidebar">
           <Sidebar />
+          <Report props={data} />
+          <Report props={data} />
         </div>
         <div className="map">
           <Map />
