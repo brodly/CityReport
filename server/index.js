3b5015167c8fea6413cbf2af4ed712712c570042
Joshua Marx <marx.joshua@gmail.com>
2018-12-04 19:12:44 -0800

    removed

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const db = require('../db/index');
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,9 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/:id', (req, res) => {
-  console.log(res);
+  db.raw(`SELECT * FROM reports WHERE id='${req.params.id}'`)
+    .then((r) => { res.send(r.rows).end(); })
+    .catch((err) => { console.log(err); });
 });
 
 app.listen(port, () => console.log(`Listing on ${port}`));
