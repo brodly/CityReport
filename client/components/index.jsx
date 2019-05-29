b63ffee29413a4ff627fc6790f276f1d35e55869
Joshua Marx <marx.joshua@gmail.com>
2019-05-29 16:55:42 -0700

    Added component to track

---
+++ client/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as MapContainer } from './MapContainer';
+export { default as SubmitForm } from './SubmitForm';
+export { default as Topbar } from './Topbar';
+export { default as Report } from './Report';
