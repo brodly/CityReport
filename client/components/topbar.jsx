53c8b1e2543edba31940f0207188b01bded1079f
Joshua Marx <marx.joshua@gmail.com>
2019-05-29 16:50:30 -0700

    Added button. OnClick displays report form

--- client/components/topbar.jsx
+++ client/components/topbar.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 
-export default class Sidebar extends Component {
+export default class Topbar extends Component {
   constructor() {
     super();
     this.state = {
 
     };
+
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onButtonClick() {
+    document.querySelector(".submit-form").style.display = "block"
   }
 
   render() {
     return (
-      <div>
+      <div className="topbar">
         <label id="report">CityReport</label>
+        <button onClick={this.onButtonClick}>Submit New Ticket</button>
       </div>
     );
   }
