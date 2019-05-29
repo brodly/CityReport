965c364b7a12e78e759954000dea3f67dc03331d
Joshua Marx <marx.joshua@gmail.com>
2019-05-29 16:53:21 -0700

    Updated API for searching MongoDB with query options

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
-const db = require('../db/index');
+const db = require('../db');
+const { Report } = require('../db/models')
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/:id', (req, res) => {
-  db.raw(`SELECT * FROM reports WHERE id='${req.params.id}'`)
-    .then((r) => { res.send(r.rows).end(); })
-    .catch((err) => { console.log(err); });
+app.get('/search', (req, res) => {
+  const search = Report.find(req.query).collation({ locale: 'en_US', strength: 2 });
+
+  search.exec((err, docs) => {
+    if (err) res.send(err)
+    else res.send(docs)
+  });
 });
 
 app.listen(port, () => console.log(`Listing on ${port}`));
