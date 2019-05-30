c504b526a8f576c6b52e64667698d2ac37765784
Joshua Marx <marx.joshua@gmail.com>
2019-05-29 18:10:01 -0700

    Added POST form submittion to server

--- client/components/SubmitForm.jsx
+++ client/components/SubmitForm.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class SubmitForm extends Component {
   constructor() {
     super();
     this.state = {
-
+      serviceslist: '',
+      fname: '',
+      lname: '',
+      email: '',
+      phone: '',
+      desc: '',
     };
 
-    this.closeForm     = this.closeForm.bind(this);
-    this.onCloseClick  = this.onCloseClick.bind(this);
-    this.onSubmitClick = this.onSubmitClick.bind(this);
+    this.onChange     = this.onChange.bind(this);
+    this.closeForm    = this.closeForm.bind(this);
+    this.onCloseClick = this.onCloseClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
   closeForm() {
     document.querySelector(".submit-form").style.display = "none";
   }
@@ -20,33 +30,44 @@ export default class SubmitForm extends Component {
     this.closeForm();
   }
 
-  onSubmitClick(e) {
-    e.preventDefault();
+  handleSubmit(e) {
     this.closeForm();
+    axios.post('/submit', this.state)
+      .then((res)=> console.log(res))
     alert('Report submitted!');
+    e.preventDefault();
   }
 
   render() {
+    const {
+      serviceslist,
+      fname,
+      lname,
+      email,
+      phone,
+      desc,
+    } = this.state;
+
     return (
       <div className="submit-form">
-        <div>
+        <div id="header">
           <label id="report">Report an issue</label>
           <button onClick={this.onCloseClick} style={{float: "right"}}>X</button>
         </div>
-        <select>
+        <select name="serviceslist" form="reportform" onChange={this.onChange} value={serviceslist}>
           <option value="pothole">Pothole</option>
           <option value="hydrant">Fire Hydrant</option>
           <option value="lamppost">Lamp Post</option>
           <option value="manhole">Manhole</option>
         </select>
         <br/>
-        <form>
-          First name: <input type="text" name="fname"/><br/>
-          Last name: <input type="text" name="lname"/><br/>
-          Email: <input type="text" name="email" /><br />
-          Telephone: <input type="text" name="telephone" /><br />
-          Description of Issue: <input type="text" name="issue" /><br />
-          <input type="submit" value="Submit" onClick={this.onSubmitClick}/>
+        <form id="reportform" onSubmit={this.handleSubmit}>
+          First name: <input type="text" name="fname" onChange={this.onChange} value={fname}/><br/>
+          Last name: <input type="text" name="lname" onChange={this.onChange} value={lname}/><br/>
+          Email: <input type="text" name="email" onChange={this.onChange} value={email}/><br />
+          Telephone: <input type="text" name="phone" onChange={this.onChange} value={phone}/><br />
+          Description of Issue: <input type="text" name="desc" onChange={this.onChange} value={desc}/><br />
+          <input type="submit" value="Submit"/>
         </form>
       </div>
 
