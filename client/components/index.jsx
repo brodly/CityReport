36959aa03cb3c9ed27b7fccc5e4e2e71d351c8c7
Joshua Marx <marx.joshua@gmail.com>
2019-06-03 16:44:56 -0700

    Added filter and search components

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -2,3 +2,5 @@ export { default as MapContainer } from './MapContainer';
 export { default as SubmitForm } from './SubmitForm';
 export { default as Topbar } from './Topbar';
 export { default as Report } from './Report';
+export { default as Search } from './Search';
+export { default as Filter } from './Filter';
