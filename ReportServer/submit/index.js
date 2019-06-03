7c66f8df064911af96141c63722affef81105c42
Joshua Marx <marx.joshua@gmail.com>
2019-06-03 16:42:42 -0700

    Basic version of handling dynamic request types

--- ReportServer/submit/index.js
+++ ReportServer/submit/index.js
@@ -1,11 +1,15 @@
-const { pothole } = require('../lib/serviceList.js');
+const services = require('../lib/serviceList.js');
 
 module.exports = {
+  services,
   'submit' : function (browser) {
+    const services = this.services;
 
     const {
       location: loc,
-      service,
+      serviceType,
+      requestType,
+      serviceLocation,
       fname,
       lname,
       email,
@@ -13,17 +17,21 @@ module.exports = {
       desc,
     } = browser.globals;
 
+    const step1 = services[requestType].select;
+    const step2 = services[requestType][serviceType];
+    const step3 = services[requestType].location[serviceLocation];
+
     browser
       .url('https://myla311.lacity.org/portal/faces/home/service/create-sr')
       .waitForElementVisible('body', 6000)
       .elements('css selector', '.af_selectBooleanRadio_native-input', (result) => {
-        const element = result.value[pothole.step1].ELEMENT;
+        const element = result.value[step1].ELEMENT;
         browser
           .pause(2000)
           .elementIdClick(element)
           .pause(2000)
           .elements('css selector', '.af_selectBooleanRadio_native-input', (result) => {
-            const element = result.value[pothole.step2].ELEMENT;
+            const element = result.value[step2].ELEMENT;
             browser
               .elementIdClick(element)
               .pause(1000)
@@ -46,7 +54,7 @@ module.exports = {
       .pause(5000)
       .waitForElementVisible('body', 6000)
       .elements('css selector', '.af_selectOneRadio_native-input', (result) => {
-        const element = result.value[pothole.step3].ELEMENT;
+        const element = result.value[step3].ELEMENT;
         browser
           .elementIdClick(element)
           .pause(1000)
