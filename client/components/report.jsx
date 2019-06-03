463de1286042eb25161b8f6555587970ce046ed0
Joshua Marx <marx.joshua@gmail.com>
2019-06-02 18:04:50 -0700

    Styled reports with open status for tickets

--- client/components/report.jsx
+++ client/components/report.jsx
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Description from '@material-ui/icons/description';
 
 export default class Report extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      index: this.props.index,
     };
+
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
   }
 
+  onMouseEnter() {
+    const { handleOnMouseEnter } = this.props;
+    const { index }              = this.state;
+
+    handleOnMouseEnter(index);
+  }
+
   render() {
-    const { address, requesttype } = this.props.report;
+    const { address, requesttype, status } = this.props.report;
 
     return (
       <div className="report">
-        <div id="title">{address}</div>
-        <div id="desc">{requesttype}</div>
-        <input onSubmit={this.handleSubmit} type="submit" value="Update" />
-        <input onSubmit={this.handleSubmit} type="submit" value="Delete" />
+        <ListItem
+          button
+          dense
+          divider
+          alignItems='center'
+          onMouseEnter={this.onMouseEnter}>
+          <ListItemIcon>
+            <Description/>
+          </ListItemIcon>
+          <ListItemText
+            primary={address}
+            secondary={requesttype} />
+          <ListSubheader
+            style={{
+              'color': 'rgba(25, 175, 61, 0.54)',
+              'background-color': 'rgb(25, 175, 61, 0.2)',
+              'line-height': '30px',
+              'border-radius': '5px',
+              'height': '30px',
+            }}>
+            {status}
+          </ListSubheader>
+        </ListItem>
       </div>
     );
   }
