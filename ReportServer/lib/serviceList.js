54e0a9604278cbb44b257f4af3a8ce59b3b35227
Joshua Marx <marx.joshua@gmail.com>
2019-06-04 14:08:06 -0700

    renamed keys to reflect serviceType value

--- ReportServer/lib/serviceList.js
+++ ReportServer/lib/serviceList.js
@@ -1,8 +1,22 @@
 module.exports = {
-  street: {
+  'Graffiti Removal': {
+    select: 'pt1:r1:0:r1:0:r1:0:s1:t1:3:it2::content',
+    'Graffiti Removal': 'pt1:r1:0:r1:0:r1:0:s1:t2:0:sbr1::content',
+    details: {
+      wall: 'pt1:r1:0:r1:0:r1:2:r1:0:r1:0:t1:38:sbc1::content',
+      select: {
+        open: 'pt1:r1:0:r1:0:r1:2:r1:0:r1:0:soc1::content',
+        other: 'Other',
+        manager: 'Manager',
+        owner: 'Owner',
+        resident: 'Resident',
+      }
+    }
+  },
+  pothole: {
     select: 'pt1:r1:0:r1:0:r1:0:s1:t1:6:it2::content',
     pothole: 'pt1:r1:0:r1:0:r1:0:s1:t2:8:sbr1::content',
-    location: {
+    details: {
       alley: 'pt1:r1:0:r1:0:r1:2:r1:0:sor1:_0',
       street: 'pt1:r1:0:r1:0:r1:2:r1:0:sor1:_1',
     }
