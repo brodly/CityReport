315e665240867f6eb4424f2d405f6b2a082bcb51
Joshua Marx <marx.joshua@gmail.com>
2019-06-04 11:12:23 -0700

    Updated street/pothole repair to point to radio id

--- ReportServer/lib/serviceList.js
+++ ReportServer/lib/serviceList.js
@@ -1,10 +1,10 @@
 module.exports = {
   street: {
-    select: 6,
-    pothole: 30,
+    select: 'pt1:r1:0:r1:0:r1:0:s1:t1:6:it2::content',
+    pothole: 'pt1:r1:0:r1:0:r1:0:s1:t2:8:sbr1::content',
     location: {
-      alley: 0,
-      street: 1,
+      alley: 'pt1:r1:0:r1:0:r1:2:r1:0:sor1:_0',
+      street: 'pt1:r1:0:r1:0:r1:2:r1:0:sor1:_1',
     }
   }
 }
