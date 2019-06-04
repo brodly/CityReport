0f3b2a0edc2e496a7f5af5fd6ce1b4e6f70fadb6
Joshua Marx <marx.joshua@gmail.com>
2019-06-04 11:14:19 -0700

    Radio buttons click use ID to locate. Pause after every click

--- ReportServer/submit/index.js
+++ ReportServer/submit/index.js
@@ -23,42 +23,28 @@ module.exports = {
 
     browser
       .url('https://myla311.lacity.org/portal/faces/home/service/create-sr')
-      .waitForElementVisible('body', 6000)
-      .elements('css selector', '.af_selectBooleanRadio_native-input', (result) => {
-        const element = result.value[step1].ELEMENT;
-        browser
-          .pause(2000)
-          .elementIdClick(element)
-          .pause(2000)
-          .elements('css selector', '.af_selectBooleanRadio_native-input', (result) => {
-            const element = result.value[step2].ELEMENT;
-            browser
-              .elementIdClick(element)
-              .pause(1000)
-              .click('.blueButton.commonButton.af_commandLink')
-
-          })
-      })
+      .waitForElementVisible('body', 5000)
+      .click('id', step1)
+      .pause(1000)
+      .click('id', step2)
+      .pause(1000)
+      .click('.blueButton.commonButton.af_commandLink')
       .pause(5000)
-      .waitForElementVisible('body', 6000)
-      .execute(function(loc){
-        document.getElementById('pt1:r1:0:r1:0:r1:1:it5::content').value = loc;
-      }, [loc])
+      .waitForElementVisible('body', 5000)
+      .execute(function(loc){ document.getElementById('pt1:r1:0:r1:0:r1:1:it5::content').value = loc }, [loc])
+      .pause(1000)
       .click('a[title="Verify Location"]')
       .pause(2000)
       .execute(function(email){ document.getElementById('pt1:r1:0:r1:0:r1:1:it1::content').value = email }, [email])
       .execute(function(fname){ document.getElementById('pt1:r1:0:r1:0:r1:1:it2::content').value = fname }, [fname])
       .execute(function(lname){ document.getElementById('pt1:r1:0:r1:0:r1:1:it3::content').value = lname }, [lname])
       .execute(function(phone){ document.getElementById('pt1:r1:0:r1:0:r1:1:it4::content').value = phone }, [phone])
+      .pause(1000)
       .click('a[title="Next"]')
       .pause(5000)
-      .waitForElementVisible('body', 6000)
-      .elements('css selector', '.af_selectOneRadio_native-input', (result) => {
-        const element = result.value[step3].ELEMENT;
-        browser
-          .elementIdClick(element)
-          .pause(1000)
-      })
+      .waitForElementVisible('body', 5000)
+      .click('id', step3)
+      .pause(1000)
       .click('a[title="Next Button"]')
       .pause(5000)
       .waitForElementVisible('body', 6000)
